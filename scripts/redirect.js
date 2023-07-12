683a7d38cab27376f33464559ddd4327b3fbcb99
BDFFZI <1330958515@qq.com>
2023-07-12 21:59:00 +0800

    -

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -3,6 +3,15 @@ let request = new XMLHttpRequest();
 request.open("HEAD", url, true);
 request.timeout = 2000;
 request.onload = () => window.location = url;
-request.ontimeout = () => window.location = 'index.html';
-request.onerror = () => window.location = 'index.html';
+request.ontimeout = () => OnFailed();
+request.onerror = () => OnFailed();
 request.send();
+
+function OnFailed()
+{
+    let loading = document.getElementById('loading');
+    let text = document.createElement('span')
+    loading.appendChild(text)
+
+    text.textContent = '加载失败';
+}
