1804dc8dd58d481af0337de4b1622a4d88241f88
BDFFZI <1330958515@qq.com>
2023-07-12 22:39:04 +0800

    -

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -5,7 +5,7 @@ let url = document.querySelector('meta[name="redirect"]').getAttribute('content'
 let request = new XMLHttpRequest();
 request.open("HEAD", url, true);
 request.timeout = 2000;
-request.onload = () => window.location = url;
+request.onload = () => OnSucceeded();
 request.ontimeout = () => OnFailed();
 request.onerror = () => OnFailed();
 request.send();
@@ -17,4 +17,15 @@ function OnFailed()
     let text = document.createElement('span')
     text.textContent = '加载失败';
     document.body.appendChild(text)
+}
+
+function OnSucceeded()
+{
+    loading.style.display = 'none'
+
+    let iframe = document.createElement(`iframe`);
+    iframe.src = url;
+    iframe.width = 1920/2;
+    iframe.height = 1080;
+    document.body.appendChild(iframe);
 }
