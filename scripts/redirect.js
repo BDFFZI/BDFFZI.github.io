024042631a6235b49833d388ab01d20d6145b218
BDFFZI <1330958515@qq.com>
2023-07-12 22:45:17 +0800

    -

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,31 +1,37 @@
-let loading = document.getElementById('loading');
-loading.style.display = 'block'
-
 let url = document.querySelector('meta[name="redirect"]').getAttribute('content');
-let request = new XMLHttpRequest();
-request.open("HEAD", url, true);
-request.timeout = 2000;
-request.onload = () => OnSucceeded();
-request.ontimeout = () => OnFailed();
-request.onerror = () => OnFailed();
-request.send();
+let iframe = document.createElement(`iframe`);
+iframe.src = url;
+iframe.width = 1920/2;
+iframe.height = 1080;
+document.body.appendChild(iframe);
+// let loading = document.getElementById('loading');
+// loading.style.display = 'block'
 
-function OnFailed()
-{
-    loading.style.display = 'none'
+// let url = document.querySelector('meta[name="redirect"]').getAttribute('content');
+// let request = new XMLHttpRequest();
+// request.open("HEAD", url, true);
+// request.timeout = 2000;
+// request.onload = () => OnSucceeded();
+// request.ontimeout = () => OnFailed();
+// request.onerror = () => OnFailed();
+// request.send();
 
-    let text = document.createElement('span')
-    text.textContent = '加载失败';
-    document.body.appendChild(text)
-}
+// function OnFailed()
+// {
+//     loading.style.display = 'none'
 
-function OnSucceeded()
-{
-    loading.style.display = 'none'
+//     let text = document.createElement('span')
+//     text.textContent = '加载失败';
+//     document.body.appendChild(text)
+// }
 
-    let iframe = document.createElement(`iframe`);
-    iframe.src = url;
-    iframe.width = 1920/2;
-    iframe.height = 1080;
-    document.body.appendChild(iframe);
-}
+// function OnSucceeded()
+// {
+//     loading.style.display = 'none'
+
+//     let iframe = document.createElement(`iframe`);
+//     iframe.src = url;
+//     iframe.width = 1920/2;
+//     iframe.height = 1080;
+//     document.body.appendChild(iframe);
+// }
