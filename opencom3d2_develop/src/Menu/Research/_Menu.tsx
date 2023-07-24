9eb9236e6c8a72ef5e3dd0a30d2f7cbbbaba6b1c
夷乐鑫 <1330958515@qq.com>
2023-07-24 12:31:57 +0800

    -

--- opencom3d2_develop/src/Menu/Research/_Menu.tsx
+++
@@ -1,9 +0,0 @@
-import { ExperimentOutlined } from "@ant-design/icons";
-import { ItemDeveloping } from "../MenuSystem";
-
-const Menu = {
-    key: '/Research',
-    label: <ItemDeveloping label="研究" />,
-    icon: <ExperimentOutlined />,
-}
-export default Menu;
--- opencom3d2_develop/src/Menu/Welcome/UpdateLog.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react"
-
-const Log: { time: string, content: ReactNode }[] = [
-    { time: "2023.7.24", content: "毛坯房变简装房啦！" },
-]
-export default Log
